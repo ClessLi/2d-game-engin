e218218dffee5e552fe45bb0b5776faba982bb6f
ClessLi <316600949@qq.com>
2020-01-09 15:23:20 +0800

    添加相关注释

--- core/scene/player.go
+++ core/scene/player.go
@@ -6,12 +6,23 @@ import (
 	"github.com/go-gl/mathgl/mgl32"
 )
 
+// Player, 玩家角色对象，暂以方形作为角色的形状对象，包含了武器类型与攻击矢量
 type Player struct {
 	resolv.Rectangle
 	weapon Weapon
 	AtkVec mgl32.Vec2
 }
 
+// NewPlayer, Player 类实例初始化函数
+// 参数:
+//     x, y: 角色坐标
+//     w, h: 角色长宽尺寸
+//     friction: 角色阻力值
+//     drawMulti: 角色渲染缩放系数
+//     moveList: 动态 Texture 对象名列表
+//     standList: 静态 Texture 对象名列表
+// 返回值:
+//     Player 类指针
 func NewPlayer(x, y, w, h int32, friction, drawMulti float32, moveList, standList []string) *Player {
 	r := resolv.NewRectangle(x, y, w, h,
 		friction, drawMulti,
@@ -24,6 +35,9 @@ func NewPlayer(x, y, w, h int32, friction, drawMulti float32, moveList, standLis
 	return p
 }
 
+// Attack, Player 类攻击方法
+// 返回值:
+//     resolve.Shape 类，武器攻击作用形状对象
 func (p *Player) Attack() resolv.Shape {
 	x, y := p.GetXY()
 	if !p.IsXReverse {
--- core/scene/weapon.go
+++ core/scene/weapon.go
@@ -7,11 +7,13 @@ import (
 	"github.com/go-gl/mathgl/mgl32"
 )
 
+// Weapon, 武器接口对象，定义武器攻击与冷却方法
 type Weapon interface {
 	Attack(X, Y int32, vec2 mgl32.Vec2, isXReverse bool) resolv.Shape
 	CoolDown(delta float64)
 }
 
+// LongRangeWeapon, 远程武器类，定义远程武器及其相关信息
 type LongRangeWeapon struct {
 	BoltName   string
 	CD         float64
@@ -20,6 +22,13 @@ type LongRangeWeapon struct {
 	BoltRadius int32
 }
 
+// Attack, LongRangeWeapon 类攻击方法， Weapon.Attack(X, Y int32, vec2 mgl32.Vec2, isXReverse bool) resolv.Shape 的实现
+// 参数:
+//     X, Y: 攻击初始坐标
+//     vec2: 攻击矢量
+//     isXReverse: 图像是水平镜像向后的
+// 返回值:
+//     resolv.Shape 类，攻击作用形状对象
 func (lw *LongRangeWeapon) Attack(X, Y int32, vec2 mgl32.Vec2, isXReverse bool) resolv.Shape {
 	if lw.CDDelta > 0 {
 		return nil
@@ -41,22 +50,32 @@ func (lw *LongRangeWeapon) Attack(X, Y int32, vec2 mgl32.Vec2, isXReverse bool) 
 	return bolt
 }
 
+// CoolDown, LongRangeWeapon 类攻击冷却方法， Weapon.CoolDown(delta float64) 的实现
+// 参数:
+//     delta: 上次更新后时延
 func (lw *LongRangeWeapon) CoolDown(delta float64) {
 	lw.CDDelta -= delta
 }
 
+// initSpd, LongRangeWeapon 类定义子弹初速度的包内方法
+// 参数:
+//     vec2: 攻击方向矢量
+// 返回值:
+//     float32, float32 类型，子弹水平与垂直方向速度值
 func (lw *LongRangeWeapon) initSpd(vec2 mgl32.Vec2) (float32, float32) {
 	vecLen := vec2.Len()
 	return vec2[0] * lw.Speed / vecLen, vec2[1] * lw.Speed / vecLen
 }
 
+// NewFireBolt, 火球武器实例化函数
+// 返回值:
+//     LongRangeWeapon 类指针
 func NewFireBolt() *LongRangeWeapon {
-	weapon := &LongRangeWeapon{
+	return &LongRangeWeapon{
 		BoltName:   "FireBolt",
 		CD:         1.0,
 		CDDelta:    0,
 		Speed:      20,
 		BoltRadius: 10,
 	}
-	return weapon
 }
